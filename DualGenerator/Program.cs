20c70187328dcc073964d1b9387c109602273e70
Marcell Veres <veresmarci@gmail.com>
2022-10-17 17:09:11 +0200

    Transpose works

--- DualGenerator/Program.cs
+++ DualGenerator/Program.cs
@@ -1,33 +1,99 @@
 ﻿
+using DualGenerator;
+
 int variables = 0;
 int conditions = 0;
 List<List<int>> x = new();
-List<List<int>> dual = new();
 List<int> b = new();
+List<Operation> operations = new();
+List<int> function = new();
+ObjectiveFunction objectiveFunction = ObjectiveFunction.Unknown;
 
-Console.WriteLine("Duál generáló");
+Console.WriteLine("Duál generáló\n");
 
-ReadInput();
-ShowConditions();
-
-void ShowConditions()
+//ReadInput();
+Random random = new();
+for (int i = 0; i < 3; i++)
 {
-    for (int i = 0; i < conditions; i++)
+    x.Add(new());
+    for (int j = 0; j < 3; j++)
     {
-        for (int j = 0; j < variables; j++)
+        x[i].Add(random.Next(-20, 20));
+    }
+
+    var rand = random.NextDouble();
+    if (rand < 1d / 3d)
+    {
+        operations.Add(Operation.GreaterThanOrEqual);
+    }
+    else if (rand < 2d / 3d)
+    {
+        operations.Add(Operation.LessThanOrEqual);
+    }
+    else
+    {
+        operations.Add(Operation.Equal);
+    }
+
+    b.Add(random.Next(-10, 10));
+    function.Add(random.Next(-10, 10));
+    objectiveFunction = random.NextDouble() > 0.5 ? ObjectiveFunction.Max : ObjectiveFunction.Min;
+}
+ShowMatrix(x, b, function);
+Console.WriteLine("\n");
+var result = GenerateDual(x);
+ShowMatrix(result, function, b);
+Console.ReadKey();
+
+
+void ShowMatrix(List<List<int>> matrix, List<int> l1, List<int> l2)
+{
+    for (int i = 0; i < matrix.Count; i++)
+    {
+        for (int j = 0; j < matrix.First().Count; j++)
         {
             string preseage = "";
-            if (j > 0 || x[i][j] < 0)
+            if (j > 0 || matrix[i][j] < 0)
             {
-                preseage = x[i][j] >= 0 ? "+" : "-";
+                preseage = matrix[i][j] >= 0 ? "+" : "-";
             }
-            Console.Write($"{preseage}{Math.Abs(x[i][j])}");
+            Console.Write($"{preseage}{Math.Abs(matrix[i][j])}");
         }
 
-        Console.Write($"<{b[i]}");
+        var operation = "";
+        switch (operations[i])
+        {
+            case Operation.GreaterThanOrEqual:
+                operation = ">=";
+                break;
+            case Operation.LessThanOrEqual:
+                operation = "<=";
+                break;
+            case Operation.Equal:
+                operation = "=";
+                break;
+            case Operation.Unknown:
+            default:
+                break;
+        }
+
+        Console.Write($" {operation} {l1[i]}");
 
         Console.WriteLine();
     }
+
+    for (int i = 0; i < l2.Count; i++)
+    {
+        string preseage = "";
+        if (i > 0 || l2[i] < 0)
+        {
+            preseage = l2[i] >= 0 ? "+" : "-";
+        }
+        Console.Write($"{preseage}{Math.Abs(l2[i])}");
+
+    }
+
+    Console.Write($"->{objectiveFunction}");
 }
 
 void ReadInput()
@@ -83,10 +149,15 @@ void ReadInput()
     }
 }
 
-void GenerateDual()
+List<List<int>> GenerateDual(List<List<int>> matrix)
 {
-    for (int i = 0; i < conditions; i++)
-    {
+    List<List<int>> matrixT = new();
 
+    for (int i = 0; i < matrix.First().Count; i++)
+    {
+        var a = matrix.Select(row => row[i]);
+        matrixT.Add(a.ToList());
     }
+
+    return matrixT;
 }
