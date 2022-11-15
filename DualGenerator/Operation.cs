c3f7165f77f6c010dddb407fa34217e9039ff321
Marcell Veres <veresmarci@bayzoltan.hu>
2022-11-15 17:36:37 +0100

    update

--- DualGenerator/Operation.cs
+++ DualGenerator/Operation.cs
@@ -1,10 +1,32 @@
 ﻿namespace DualGenerator
 {
-    enum Operation
+    public enum Operation
     {
         Unknown = 0,
         GreaterThanOrEqual,
         LessThanOrEqual,
-        Equal
+        Equal,
+        Arbitrary,
+    }
+
+    public static class OperationHelper
+    {
+        public static string ToString(Operation operation)
+        {
+            switch (operation)
+            {
+                case Operation.GreaterThanOrEqual:
+                    return ">=";
+                case Operation.LessThanOrEqual:
+                    return "<=";
+                case Operation.Equal:
+                    return "=";
+                case Operation.Arbitrary:
+                    return "tetszőleges";
+                case Operation.Unknown:
+                default:
+                    throw new ArgumentException();
+            }
+        }
     }
 }
--- DualGenerator/Program.cs
+++ DualGenerator/Program.cs
@@ -1,106 +1,39 @@
-﻿
-using DualGenerator;
+﻿using DualGenerator;
 
 int variables = 0;
 int conditions = 0;
-List<List<int>> x = new();
+
+List<List<int>> Ax = new();
+List<Operation> bConditions = new();
 List<int> b = new();
-List<Operation> operations = new();
-List<int> function = new();
+
+List<int> x = new();
+List<Operation> xConditions = new();
 ObjectiveFunction objectiveFunction = ObjectiveFunction.Unknown;
 
 Console.WriteLine("Duál generáló\n");
 
 //ReadInput();
-Random random = new();
-for (int i = 0; i < 3; i++)
-{
-    x.Add(new());
-    for (int j = 0; j < 3; j++)
-    {
-        x[i].Add(random.Next(-20, 20));
-    }
+GenerateInput();
 
-    var rand = random.NextDouble();
-    if (rand < 1d / 3d)
-    {
-        operations.Add(Operation.GreaterThanOrEqual);
-    }
-    else if (rand < 2d / 3d)
-    {
-        operations.Add(Operation.LessThanOrEqual);
-    }
-    else
-    {
-        operations.Add(Operation.Equal);
-    }
+ShowResult(Ax, bConditions, b, x, xConditions, objectiveFunction);
+Console.WriteLine("\n\n");
 
-    b.Add(random.Next(-10, 10));
-    function.Add(random.Next(-10, 10));
-    objectiveFunction = random.NextDouble() > 0.5 ? ObjectiveFunction.Max : ObjectiveFunction.Min;
-}
-ShowMatrix(x, b, function);
-Console.WriteLine("\n");
-var result = GenerateDual(x);
-ShowMatrix(result, function, b);
+var Bx = Transpose(Ax);
+var newBConditions = GetNewBConditions(xConditions);
+var newXConditions = GetNewXConditions(bConditions);
+objectiveFunction = GetDualObjectiveFunction(objectiveFunction);
+
+ShowResult(Bx, newBConditions, x, b, newXConditions, objectiveFunction);
 Console.ReadKey();
 
 
-void ShowMatrix(List<List<int>> matrix, List<int> l1, List<int> l2)
-{
-    for (int i = 0; i < matrix.Count; i++)
-    {
-        for (int j = 0; j < matrix.First().Count; j++)
-        {
-            string preseage = "";
-            if (j > 0 || matrix[i][j] < 0)
-            {
-                preseage = matrix[i][j] >= 0 ? "+" : "-";
-            }
-            Console.Write($"{preseage}{Math.Abs(matrix[i][j])}");
-        }
-
-        var operation = "";
-        switch (operations[i])
-        {
-            case Operation.GreaterThanOrEqual:
-                operation = ">=";
-                break;
-            case Operation.LessThanOrEqual:
-                operation = "<=";
-                break;
-            case Operation.Equal:
-                operation = "=";
-                break;
-            case Operation.Unknown:
-            default:
-                break;
-        }
-
-        Console.Write($" {operation} {l1[i]}");
-
-        Console.WriteLine();
-    }
-
-    for (int i = 0; i < l2.Count; i++)
-    {
-        string preseage = "";
-        if (i > 0 || l2[i] < 0)
-        {
-            preseage = l2[i] >= 0 ? "+" : "-";
-        }
-        Console.Write($"{preseage}{Math.Abs(l2[i])}");
-
-    }
-
-    Console.Write($"->{objectiveFunction}");
-}
 
 void ReadInput()
 {
     Console.Write("Adja meg a változók számát: ");
 
-    if (!int.TryParse(Console.ReadLine().Trim(), out variables) || variables < 0)
+    if (!int.TryParse(Console.ReadLine()?.Trim(), out variables) || variables < 0)
     {
         Console.WriteLine("Hibás formátum!");
         return;
@@ -108,7 +41,7 @@ void ReadInput()
 
     Console.Write("Adja meg a feltételek számát: ");
 
-    if (!int.TryParse(Console.ReadLine().Trim(), out conditions) || conditions < 0)
+    if (!int.TryParse(Console.ReadLine()?.Trim(), out conditions) || conditions < 0)
     {
         Console.WriteLine("Hibás formátum!");
         return;
@@ -122,24 +55,24 @@ void ReadInput()
 
     for (int i = 0; i < conditions; i++)
     {
-        x.Add(new());
+        Ax.Add(new());
         for (int j = 0; j < variables; j++)
         {
             int xj;
             Console.Write($"x{j}: ");
 
-            if (!int.TryParse(Console.ReadLine().Trim(), out xj))
+            if (!int.TryParse(Console.ReadLine()?.Trim(), out xj))
             {
                 Console.WriteLine("Hibás formátum!");
                 return;
             }
 
-            x[i].Add(xj);
+            Ax[i].Add(xj);
         }
 
         Console.Write($"Condition: < ");
         int bi;
-        if (!int.TryParse(Console.ReadLine().Trim(), out bi))
+        if (!int.TryParse(Console.ReadLine()?.Trim(), out bi))
         {
             Console.WriteLine("Hibás formátum!");
             return;
@@ -148,8 +81,40 @@ void ReadInput()
         b.Add(bi);
     }
 }
+void GenerateInput()
+{
+    Random random = new();
 
-List<List<int>> GenerateDual(List<List<int>> matrix)
+    for (int i = 0; i < 3; i++)
+    {
+        Ax.Add(new());
+        for (int j = 0; j < 3; j++)
+        {
+            Ax[i].Add(random.Next(-20, 20));
+        }
+
+        var rand = random.NextDouble();
+        if (rand < 1d / 3d)
+        {
+            bConditions.Add(Operation.GreaterThanOrEqual);
+        }
+        else if (rand < 2d / 3d)
+        {
+            bConditions.Add(Operation.LessThanOrEqual);
+        }
+        else
+        {
+            bConditions.Add(Operation.Equal);
+        }
+
+        b.Add(random.Next(-10, 10));
+        x.Add(random.Next(-10, 10));
+        xConditions.Add(Operation.GreaterThanOrEqual);
+        objectiveFunction = random.NextDouble() > 0.5 ? ObjectiveFunction.Max : ObjectiveFunction.Min;
+    }
+}
+
+List<List<int>> Transpose(List<List<int>> matrix)
 {
     List<List<int>> matrixT = new();
 
@@ -160,4 +125,117 @@ List<List<int>> GenerateDual(List<List<int>> matrix)
     }
 
     return matrixT;
+}
+//TODO test
+List<Operation> GetNewBConditions(List<Operation> list)
+{
+    List<Operation> result = new();
+
+    if (list.Any(x => x == Operation.Unknown))
+    {
+        throw new ArgumentException();
+    }
+
+    foreach (var op in list)
+    {
+        if (op == Operation.Equal)
+        {
+            result.Add(Operation.Arbitrary);
+        }
+        else if (op == Operation.Arbitrary)
+        {
+            result.Add(Operation.Equal);
+        }
+
+        result.Add(op);
+    }
+
+    return result;
+}
+List<Operation> GetNewXConditions(List<Operation> list)
+{
+    List<Operation> result = new();
+
+    if (list.Any(x => x == Operation.Unknown))
+    {
+        throw new ArgumentException();
+    }
+
+    foreach (var op in list)
+    {
+        if (op == Operation.Equal)
+        {
+            result.Add(Operation.Arbitrary);
+        }
+        else if (op == Operation.Arbitrary)
+        {
+            result.Add(Operation.Equal);
+        }
+
+        result.Add(op);
+    }
+
+    return result;
+}
+
+ObjectiveFunction GetDualObjectiveFunction(ObjectiveFunction objectiveFunction)
+{
+    ObjectiveFunction newFunction;
+
+    switch (objectiveFunction)
+    {
+        case ObjectiveFunction.Max:
+            newFunction = ObjectiveFunction.Min;
+            break;
+        case ObjectiveFunction.Min:
+            newFunction = ObjectiveFunction.Max;
+            break;
+        default:
+            newFunction = ObjectiveFunction.Unknown;
+            break;
+    }
+
+    return newFunction;
+}
+
+
+void ShowResult(List<List<int>> Ax, List<Operation> bOperations, List<int> b, List<int> x, List<Operation> xConditions, ObjectiveFunction objectiveFunction)
+{
+    for (int i = 0; i < Ax.Count; i++)
+    {
+        for (int j = 0; j < Ax.First().Count; j++)
+        {
+            string preseage = "";
+            string space = (j == 0 ? "" : " ");
+            if (j > 0 || Ax[i][j] < 0)
+            {
+                preseage = Ax[i][j] >= 0 ? $"+{space}" : $"-{space}";
+            }
+            Console.Write($"{preseage}{Math.Abs(Ax[i][j])} * x{j + 1} ");
+        }
+
+        var operation = OperationHelper.ToString(bOperations[i]);
+
+        Console.Write($" {operation} {b[i]}\n");
+    }
+
+    Console.WriteLine();
+    for (int i = 0; i < x.Count; i++)
+    {
+        string preseage = "";
+        string space = (i == 0 ? "" : " ");
+        if (i > 0 || x[i] < 0)
+        {
+            preseage = x[i] >= 0 ? $"+{space}" : $"-{space}";
+        }
+        Console.Write($"{preseage}{Math.Abs(x[i])} * x{i + 1} ");
+    }
+    Console.Write($"-> {objectiveFunction}\n");
+
+    for (int i = 0; i < x.Count; i++)
+    {
+        var arbitrary = xConditions[i] == Operation.Arbitrary;
+
+        Console.Write($"x{i + 1} {OperationHelper.ToString(xConditions[i])}{(arbitrary ? "" : " 0")}, ");
+    }
 }
